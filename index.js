da347c4d99366ab76bb826b847bdec304559f45f
Andrew Rowe <rowead@gmail.com>
2021-11-01 23:06:35 +0800

    basic folder processing

--- index.js
+++ index.js
@@ -1,23 +1,26 @@
 #!/usr/bin/env node
 const args = require('./args');
+const fs = require('fs');
+var path = require('path');
 
 if (args.debug) {
   console.log(args)
 }
-async function quickstart() {
-  // Imports the Google Cloud client library
-  const vision = require('@google-cloud/vision');
 
-  // Creates a client
-  const client = new vision.ImageAnnotatorClient();
+(async () => {
+  try {
+    const files = fs.readdirSync(args.path);
+    console.log(files);
 
-  // Performs label detection on the image file
-  const [result] = await client.labelDetection(args._[0]);
-  const labels = result.labelAnnotations;
-  console.log('Labels:');
-  labels.forEach(label => console.log(label.description));
-}
+    const vision = require('@google-cloud/vision');
+    const client = new vision.ImageAnnotatorClient();
 
-if (args._) {
-  quickstart();
-}
+    for (const file of files) {
+      const [result] = await client.labelDetection(path.join(args.path, file));
+      const labels = result.labelAnnotations;
+      console.log(path.join(args.path, file) + ' Labels:');
+      labels.forEach(label => console.log(label.description));
+    }
+  } catch {
+  }
+})();
