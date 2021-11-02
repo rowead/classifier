71b82ae3d8ea70f5965a9b9c39be859aea24c56a
Andrew Rowe <rowead@gmail.com>
2021-11-02 09:53:08 +0800

    skipping non-compatible images, adding local cache

--- index.js
+++ index.js
@@ -3,6 +3,12 @@ const args = require('./args');
 const fs = require('fs');
 var path = require('path');
 
+const imageTypes = [
+  '.jpeg',
+  '.jpg',
+  '.png'
+];
+
 if (args.debug) {
   console.log(args)
 }
@@ -16,10 +22,23 @@ if (args.debug) {
     const client = new vision.ImageAnnotatorClient();
 
     for (const file of files) {
-      const [result] = await client.labelDetection(path.join(args.path, file));
-      const labels = result.labelAnnotations;
-      console.log(path.join(args.path, file) + ' Labels:');
-      labels.forEach(label => console.log(label.description));
+      let labels = {};
+      if (imageTypes.includes(path.extname(file))) {
+        if (!args.force && files.includes(file + ".json")) {
+          console.log("Already processed: " + file);
+          let rawdata = fs.readFileSync(path.join(args.path, file));
+          labels = await JSON.parse(rawdata);
+        } else {
+          const [result] = await client.labelDetection(path.join(args.path, file));
+          labels = result.labelAnnotations;
+          console.log(path.join(args.path, file) + ' Labels:');
+          fs.writeFileSync(path.join(args.path, file) + '.json', JSON.stringify(labels, null, 2),);
+          // labels.forEach(label => console.log(label.description));
+        }
+        labels.forEach(label => console.log(label.description));
+      } else {
+        console.log("Skipping: " + file + "extension " + path.extname(file));
+      }
     }
   } catch {
   }
