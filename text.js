490e4aba2219e8dd81d97c29ca06957c6ec76625
Andrew Rowe <rowead@gmail.com>
2021-11-05 20:49:32 +0800

    adding test script to use language api on csv

---
+++ text.js
@@ -0,0 +1,40 @@
+#!/usr/bin/env node
+const language = require('@google-cloud/language');
+const fs = require("fs");
+const path = require("path");
+const args = require("./args");
+const csv = require('@fast-csv/parse');
+
+(async () => {
+  try {
+    // Creates a client
+    const client = new language.LanguageServiceClient();
+    let entities = [];
+    let csvStream = csv.parseFile('dwyer-head.csv', { headers: true })
+      .on('error', error => console.error(error))
+      .on('data', async row => {
+        csvStream.pause();
+        if (!args.force) {
+          let rawdata = fs.readFileSync(`./dwyer-head.csv-${row.id}.json`);
+          entities = JSON.parse(rawdata);
+          console.log(`loaded ${row.id} from cache`);
+        } else {
+          const document = {
+            content: row.Description,
+            type: 'PLAIN_TEXT',
+          };
+          entities = await client.analyzeEntities({document});
+          fs.writeFileSync(`./dwyer-head.csv-${row.id}.json`, JSON.stringify(entities, null, 2),);
+          console.log(`loaded ${row.id} from API`);
+        }
+        row.entities = [];
+        entities.entities.forEach(entity => row.entities.push({name: entity.name, type: entity.type }));
+        // console.log(`${row.id} - ${row.Title}\n--\n${row.Description}\n` + JSON.stringify(row.entities, null, 2));
+        csvStream.resume();
+      })
+      .on('end', rowCount => console.log(`Parsed ${rowCount} rows`));
+
+  } catch (error) {
+    console.log(error);
+  }
+})();
